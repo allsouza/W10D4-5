2af93a1c73444876d946477c98c9d74e4750807e
Andre <andre.lls@hotmail.com>
2020-09-10 17:22:55 -0400

    Done day 1

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 const App = () => {
-
+    return <h1>Whatever for now</h1>
 }
+
+export default App;
--- todo_redux.jsx
+++ todo_redux.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import configureStore from "./frontend/store/store";
 // import TodosApp from "./frontend/components"
 import {receiveTodo, receiveTodos} from "./frontend/actions/todo_actions"
+import Root from "./frontend/components/root"
 
 
 document.addEventListener("DOMContentLoaded", () =>{
@@ -12,5 +13,5 @@ document.addEventListener("DOMContentLoaded", () =>{
     window.receiveTodos = receiveTodos;
     window.store = store;
 
-    ReactDOM.render(<h1>Hello darkness my old friend</h1>, root)
+    ReactDOM.render(<Root store={store} />, root)
 })
